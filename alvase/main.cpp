4947d04591e9fff3653e6397159365958111eddc
DeltaStruct <DeltaStruct47@gmail.com>
2024-06-18 11:30:14 +0000

    implementation lexical analysis #1

--- alvase/main.cpp
+++ alvase/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <set>
 #include <fstream>
+#include <algorithm>
 
 struct arguments {
 
@@ -19,7 +20,61 @@ struct arguments {
 
 } args;
 
+enum class char_type {
+  number,   // 数字
+  alphabet, // アルファベット
+  symbol,   // 記号
+  unknown   // ???
+};
+
+std::string symbol_list = "!\"#%&'()*+,-./:;<=>?@[\\]^_{|}~";
+
+enum class token_type {
+  number,    // 数値
+  char_,      // 文字
+  string,    // 文字列
+  operator_, // 演算子
+  variables, // 変数
+  unknown    // ???
+};
+
 int exit_code = 0; std::string filename; std::vector<std::string> alvs_code;
+
+std::vector<std::pair<token_type,std::string>> lexical_analysis_result;
+
+void lexical_analysis(){
+  std::sort(symbol_list.begin(),symbol_list.end());
+  for (const std::string& line:alvs_code){
+    lexical_analysis_result.emplace_back(token_type::unknown,"");
+    for (char c:line){
+      char_type ct = char_type::unknown;
+      if ('a'<=c&&c<='z'){
+        ct = char_type::alphabet;
+      } else if ('A'<=c&&c<='Z'){
+        ct = char_type::alphabet;
+      } else if ('0'<=c&&c<='9'){
+        ct = char_type::number;
+      } else if (std::binary_search(symbol_list.begin(),symbol_list.end(),c)){
+        ct = char_type::symbol;
+      } else {
+        ct = char_type::unknown;
+      }
+      decltype(lexical_analysis_result)::value_type& last = lexical_analysis_result.back();
+      if (ct==char_type::unknown){
+        lexical_analysis_result.emplace_back(token_type::unknown,"");
+      } else if (last.first==token_type::string){
+        last.second.push_back(c);
+      } else if (ct==char_type::symbol&&c=='"'){
+        if (last.first==token_type::string) lexical_analysis_result.emplace_back(token_type::unknown,"");
+        else lexical_analysis_result.emplace_back(token_type::string,"");
+      } else if (ct==char_type::symbol&&c=='\''){
+        if (last.first==token_type::char_) lexical_analysis_result.emplace_back(token_type::unknown,"");
+        else lexical_analysis_result.emplace_back(token_type::char_,"");
+      } else if (last.first==token_type::variables){
+      }
+    }
+  }
+}
 
 int main(int argc, char* argv[]){
   args.set_arg(argc,argv);
@@ -35,5 +90,6 @@ int main(int argc, char* argv[]){
     std::cout << "Input args test [because --codeinput_test is setting]" << std::endl;
     for (auto& a:alvs_code) std::cout << a << std::endl;
   }
+  lexical_analysis();
   return exit_code;
 }
